c13a59a8f8685db0dc57ea8519da332d2f92168d
amber-hamlin <97491407+amber-hamlin@users.noreply.github.com>
2024-04-05 10:49:11 -0500

    sha256

--- application_processor/inc/simple_crypto.h
+++ application_processor/inc/simple_crypto.h
@@ -21,7 +21,7 @@
 /******************************** MACRO DEFINITIONS ********************************/
 #define BLOCK_SIZE AES_BLOCK_SIZE
 #define KEY_SIZE 16
-#define HASH_SIZE 224
+#define HASH_SIZE WC_SHA256_DIGEST_SIZE
 //#define HASH_SIZE WC_SHA224_DIGEST_SIZE
 
 /******************************** FUNCTION PROTOTYPES ********************************/
